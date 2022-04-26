6ca29eeaa3f6683a126c2cd51d2a93536bb5ac78
Nikita Melnik <nikitamelnikwork@gmail.com>
2022-04-26 17:27:45 +0300

    Implemented scss language preprocessing.

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,6 @@ export default {
   <router-view/>
 </template>
 
-<style>
-@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-@import "css/fonts.css";
-@import "css/global.css";
+<style lang="scss">
+@import "main";
 </style>
