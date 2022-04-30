ebb61b211e6bc08d781b1cfedd8cfb0897369056
Nikita Melnik <nikitamelnikwork@gmail.com>
2022-04-30 21:23:44 +0300

    Updated build config.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
-import store from "./store";
+import store from "@/store";
 
 export default {
   components: {
