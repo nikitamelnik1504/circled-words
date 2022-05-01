cc8f09eb9b58caf35fb4df32db094b7c565de428
Nikita Melnik <nikitamelnikwork@gmail.com>
2022-05-01 15:20:44 +0300

    Installed ESLint plugin for web page.

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
