3f052efc8d7047a00d567f1716f19166dc41e1c7
Nikita Melnik <nikitamelnikwork@gmail.com>
2022-05-01 18:36:07 +0300

    Created 404 page.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "@/views/page/Home/index.vue";
 import MyWords from "@/views/page/MyWords/index.vue";
 import Roadmap from "@/views/page/Roadmap/index.vue";
 import Error403 from "@/views/error-page/403/index.vue";
+import Error404 from "@/views/error-page/404/index.vue";
 
 const routes = [
   {
@@ -27,6 +28,15 @@ const routes = [
     path: "/403-error",
     name: "403 error",
     component: Error403,
+  },
+  {
+    path: "/404-error",
+    name: "404 error",
+    component: Error404,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404-error",
   },
 ];
 
---
+++ src/views/error-page/404/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="text-center d-flex align-items-center justify-content-center"
+        :style="{ 'min-height': freeHeight + 'px' }"
+      >
+        <h1 class="error-text">Error 404. Page not found</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getFreeHeight } from "@/utils/layout-space.js";
+
+export default {
+  name: "404Page",
+  data() {
+    return {
+      freeHeight: Number,
+    };
+  },
+  mounted() {
+    this.onResize();
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+  methods: {
+    onResize() {
+      this.freeHeight = getFreeHeight(true);
+    },
+  },
+};
+</script>
