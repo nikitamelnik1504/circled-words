b8ae77aa31278ab6f0eb52bb46e5577400f9c205
Nikita <nikitamelnikwork@gmail.com>
2022-07-28 12:09:15 +0300

    Implemented typescript support.

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ import "@popperjs/core/dist/umd/popper.min.js";
 
 const app = createApp(App);
 router.beforeEach(async (to) => {
-  const { requiresAuth } = to.meta;
+    const { requiresAuth } = to.meta;
 
-  if (requiresAuth) {
-    if (
-      (await store.getters.isMetamaskConnected) === false &&
-      (await store.getters.isWalletConnectConnected) === false
-    ) {
-      return "/403-error";
+    if (requiresAuth) {
+        if (
+            (await store.getters.isMetamaskConnected) === false &&
+            (await store.getters.isWalletConnectConnected) === false
+        ) {
+            return "/403-error";
+        }
     }
-  }
 });
 app.use(router);
 app.use(store);
