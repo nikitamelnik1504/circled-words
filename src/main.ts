374f0afbd1f7c6d8b6b6fd22eddc8b4315c4eaa8
108ddy <mgbemena900@gmail.com>
2022-08-07 16:36:44 +0300

    cw-10 Refactored Router to typescript.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
-import router from "./router";
+import router from "@/router";
 import "@popperjs/core/dist/umd/popper.min.js";
 
 const app = createApp(App);
