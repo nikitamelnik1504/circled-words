eb826ccae8d72194989e43b0537e5e77e3c03e0e
108ddy <mgbemena900@gmail.com>
2022-08-07 17:31:44 +0300

    CW-11 Refactored Store to typescript.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store";
+import store from "@/store";
 import router from "@/router";
 import "@popperjs/core/dist/umd/popper.min.js";
 
