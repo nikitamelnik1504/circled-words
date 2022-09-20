ed1e9a4dc6cba478a1e20972cc0bd90bb91b0a8a
Nikita <nikitamelnikwork@gmail.com>
2022-09-20 14:50:09 +0300

    CW-66 Fixed labels size bug of footer on mobile screen.

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <footer id="footer">
     <div class="container-fluid">
-      <div class="row py-4">
-        <div class="col-6 px-5 mx-auto">
+      <div class="row py-4 w-100 mx-auto">
+        <div class="col-7 col-sm-6 px-md-5 mx-auto">
           <h3 class="m-0 copyright">© 2022 CircledWords</h3>
         </div>
-        <div class="col-6 px-5 mx-auto text-end">
+        <div class="col-5 col-sm-6 px-md-5 mx-auto text-end">
           <h3 class="m-0 version">Version: {{ version }}</h3>
         </div>
       </div>
