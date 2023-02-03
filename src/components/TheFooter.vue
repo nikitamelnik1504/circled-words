b50ab7b49b98b476e79b86efe2af19e06512585c
nikita <nikitamelnikwork@gmail.com>
2023-02-03 13:28:30 +0200

    Added mentions of Metaplex and Solana technologies in the footer.

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,11 +1,42 @@
 <template>
   <footer id="footer">
     <div class="container-fluid">
-      <div class="row py-4 w-100 mx-auto">
-        <div class="col-7 col-sm-6 px-md-5 mx-auto">
+      <div class="row py-3 py-md-4 w-100 mx-auto justify-content-center">
+        <div
+          class="col-md-auto col-xl-4 px-lg-5 me-auto mx-lg-auto d-flex align-items-center justify-content-center justify-content-md-start order-1 order-md-0"
+        >
           <h3 class="m-0 copyright">© 2022 CircledWords</h3>
         </div>
-        <div class="col-5 col-sm-6 px-md-5 mx-auto text-end">
+        <div
+          class="col-md-auto col-xl-4 col-xxl-3 position-relative partners my-3 my-md-0 order-0 order-md-2"
+        >
+          <h3 class="mb-0 position-absolute start-0 end-0 text-center">
+            Developed with using technologies of
+          </h3>
+          <div class="row justify-content-center">
+            <div class="col-auto text-center">
+              <a href="https://www.metaplex.com" target="_blank">
+                <img
+                  src="../assets/images/metaplex.svg"
+                  height="14"
+                  alt="metaplex-logo"
+                />
+              </a>
+            </div>
+            <div class="col-auto text-center">
+              <a href="https://solana.com" target="_blank">
+                <img
+                  src="../assets/images/solana.png"
+                  height="17"
+                  alt="solana-logo"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          class="col-md-auto col-xl-4 px-lg-5 ms-auto mx-lg-auto text-end d-flex align-items-center justify-content-center justify-content-md-end mt-1 mt-md-0 order-2"
+        >
           <h3 class="m-0 version">Version: {{ version }}</h3>
         </div>
       </div>
