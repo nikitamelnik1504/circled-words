5f76d2a2b96ce45f66c521098258930348b571f2
nikita <nikitamelnikwork@gmail.com>
2023-02-25 21:14:08 +0200

    Code styles.

--- src/components/EthereumWalletModal.vue
+++ src/components/EthereumWalletModal.vue
@@ -1,104 +1,105 @@
 <template>
+  <div
+    id="connectEthereumWalletModal"
+    ref="connectEthereumWalletModal"
+    class="modal fade"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
     <div
-      id="connectEthereumWalletModal"
-      ref="connectEthereumWalletModal"
-      class="modal fade"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
+      class="modal-dialog position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center"
     >
-      <div
-        class="modal-dialog position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center"
-      >
-        <div class="modal-content px-md-3">
-          <div class="modal-header justify-content-center">
-            <h5 id="exampleModalLabel" class="modal-title">Select your wallet</h5>
-            <button
-              ref="closeWalletModal"
-              type="button"
-              class="btn-close d-none"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            />
-          </div>
-          <div class="modal-body d-flex justify-content-center">
-            <div class="row flex-column flex-sm-row w-100">
-              <div class="blockchain-wrapper">
-                <div class="row text-center h-100">
-                  <div class="col-sm-11 wallet-links-wrapper mx-auto p-3 p-sm-4">
-                    <div class="row">
-                      <div class="mb-2">
-                        <h3 class="blockchain-title ethereum">
-                          Ethereum<br />(deprecated)
-                        </h3>
-                      </div>
-                      <div class="col-6 mx-auto">
-                        <a
-                          :href="
-                            metamaskService
-                              ? '#'
-                              : 'https://metamask.io/download/'
-                          "
-                          class="wallet-link metamask-link h-100 d-flex justify-content-between align-items-center flex-column text-center position-relative py-2"
-                          @click="showMetamaskModal($event)"
+      <div class="modal-content px-md-3">
+        <div class="modal-header justify-content-center">
+          <h5 id="exampleModalLabel" class="modal-title">Select your wallet</h5>
+          <button
+            ref="closeWalletModal"
+            type="button"
+            class="btn-close d-none"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          />
+        </div>
+        <div class="modal-body d-flex justify-content-center">
+          <div class="row flex-column flex-sm-row w-100">
+            <div class="blockchain-wrapper">
+              <div class="row text-center h-100">
+                <div class="col-sm-11 wallet-links-wrapper mx-auto p-3 p-sm-4">
+                  <div class="row">
+                    <div class="mb-2">
+                      <h3 class="blockchain-title ethereum">
+                        Ethereum<br />(deprecated)
+                      </h3>
+                    </div>
+                    <div class="col-6 mx-auto">
+                      <a
+                        :href="
+                          metamaskService
+                            ? '#'
+                            : 'https://metamask.io/download/'
+                        "
+                        class="wallet-link metamask-link h-100 d-flex justify-content-between align-items-center flex-column text-center position-relative py-2"
+                        @click="showMetamaskModal($event)"
+                      >
+                        <!--            <span class="position-absolute wallet-network-error-background"-->
+                        <!--                 ></span>-->
+                        <div
+                          class="image-wrapper d-flex justify-content-center py-2"
                         >
-                          <!--            <span class="position-absolute wallet-network-error-background"-->
-                          <!--                 ></span>-->
-                          <div
-                            class="image-wrapper d-flex justify-content-center py-2"
-                          >
-                            <img
-                              src="../assets/images/metamask.svg"
-                              class="w-100"
-                              alt="metamask"
-                            />
-                          </div>
-                          <h6>Metamask</h6>
-                          <!--            <p class="wallet-network-error" >Please change your network to-->
-                          <!--              Ethereum</p>-->
-                        </a>
-                      </div>
-                      <div class="col-6 mx-auto">
-                        <a
-                          href="#"
-                          class="wallet-link wallet-connect-link h-100 d-flex justify-content-between align-items-center flex-column text-center position-relative p-2"
-                          @click.prevent="showWalletConnectModal()"
+                          <img
+                            src="../assets/images/metamask.svg"
+                            class="w-100"
+                            alt="metamask"
+                          />
+                        </div>
+                        <h6>Metamask</h6>
+                        <!--            <p class="wallet-network-error" >Please change your network to-->
+                        <!--              Ethereum</p>-->
+                      </a>
+                    </div>
+                    <div class="col-6 mx-auto">
+                      <a
+                        href="#"
+                        class="wallet-link wallet-connect-link h-100 d-flex justify-content-between align-items-center flex-column text-center position-relative p-2"
+                        @click.prevent="showWalletConnectModal()"
+                      >
+                        <span
+                          :class="{ 'd-none': walletConnectService }"
+                          class="position-absolute wallet-loader top-0 bottom-0 end-0 start-0 d-flex justify-content-center align-items-center"
+                          >Loading...</span
                         >
-                      <span
-                        :class="{ 'd-none': walletConnectService }"
-                        class="position-absolute wallet-loader top-0 bottom-0 end-0 start-0 d-flex justify-content-center align-items-center"
-                      >Loading...</span>
-                          <div
-                            class="image-wrapper d-flex justify-content-center py-2"
-                          >
-                            <img
-                              class="w-100"
-                              src="../assets/images/walletconnect.svg"
-                              alt="walletconnect"
-                            />
-                          </div>
-                          <h6>WalletConnect</h6>
-                        </a>
-                      </div>
+                        <div
+                          class="image-wrapper d-flex justify-content-center py-2"
+                        >
+                          <img
+                            class="w-100"
+                            src="../assets/images/walletconnect.svg"
+                            alt="walletconnect"
+                          />
+                        </div>
+                        <h6>WalletConnect</h6>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="modal-footer justify-content-center">
-            <button
-              type="button"
-              class="px-5 py-2"
-              data-bs-toggle="modal"
-              data-bs-target="#connectSolanaWalletModal"
-              @click="props.toggleWalletState()"
-            >
-              I'm Solana user
-            </button>
-          </div>
+        </div>
+        <div class="modal-footer justify-content-center">
+          <button
+            type="button"
+            class="px-5 py-2"
+            data-bs-toggle="modal"
+            data-bs-target="#connectSolanaWalletModal"
+            @click="props.toggleWalletState()"
+          >
+            I'm Solana user
+          </button>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -107,12 +108,11 @@ import type MetamaskService from "@/utils/Service/MetamaskService";
 import type WalletConnectService from "@/utils/Service/WalletConnectService";
 import { ref, inject } from "vue";
 
-const props = defineProps({
-  toggleWalletState: {
-    type: Function,
-    required: true,
-  },
-});
+interface Props {
+  toggleWalletState: void;
+}
+
+const props = defineProps<Props>();
 
 const metamaskService = inject<Ref<MetamaskService | false>>("metamaskService");
 
