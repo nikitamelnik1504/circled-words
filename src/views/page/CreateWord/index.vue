c2dc81779519be559ff209199e64351ba7558215
nikita <nikitamelnikwork@gmail.com>
2023-02-26 02:22:59 +0200

    Enabled dev mint.

--- src/views/page/CreateWord/index.vue
+++ src/views/page/CreateWord/index.vue
@@ -201,9 +201,9 @@
                     href="#"
                     class="py-3 text-center text-decoration-none w-50 mint-action animate__animated animate__fadeInUp"
                     :class="{
-                      disabled: true || mintRunning || !metaplexService,
+                      disabled: mintRunning || !metaplexService,
                     }"
-                    @click.prevent="() => (mintRunning ? undefined : undefined)"
+                    @click.prevent="() => (mintRunning ? undefined : mint())"
                     >Mint</a
                   >
                 </div>
